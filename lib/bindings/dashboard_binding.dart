1b352b27caf3e6ab7e9eeee97527f91f3fa1a6c1
Arya <divaarya055@gmail.com>
2025-09-23 22:09:50 +0700

    Feat [DA] Revisi page

--- lib/bindings/dashboard_binding.dart
+++ lib/bindings/dashboard_binding.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 import 'package:todo_list_app_with_category/controller/dashboard_controller.dart';
 import 'package:todo_list_app_with_category/controller/history_controller.dart';
 import 'package:todo_list_app_with_category/controller/home_controller.dart';
-import 'package:todo_list_app_with_category/controller/login_controller.dart';
 
 class DashboardBinding extends Bindings {
   @override
